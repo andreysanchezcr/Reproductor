8cfaacf7473418b75973f672c6306b90bd7372e4
Nenis <sear12@hotmail.es>
2014-08-24 08:17:25 -0600

    Se agregó la duracion de las canciones

--- MetaDatos1.java
+++ MetaDatos1.java
@@ -1,4 +1,3 @@
-//package com.mpatric.mp3agic.app;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -16,11 +15,23 @@ public class MetaDatos1{
     Mp3File mp3file;
     ID3v1 informacion;
 
-	public MetaDatos1(){
+    public MetaDatos1(){
         
         try{
-            mp3file = new Mp3File("/home/andrey/Desktop/afterlife1.mp3");//Prueba de impresion
+            mp3file = new Mp3File("/home/silvia/Dropbox/syl/musik/bachata/ansiedad - Torito.mp3");//Prueba de impresion
             informacion = mp3file.getId3v1Tag();
+            long num = mp3file.getLengthInSeconds();
+
+            long hor,min,seg;
+            String tiempo;   
+            hor=num/3600;  
+            min=(num-(3600*hor))/60;  
+            seg=num-((hor*3600)+(min*60)); 
+
+            tiempo = hor+"h:"+min+"m:"+seg+"s";
+
+            System.out.println("Length of this mp3 is: " + tiempo  + " seconds");
+            System.out.println("Bitrate: " + mp3file.getLengthInSeconds() + " kbps " + (mp3file.isVbr() ? "(VBR)" : "(CBR)"));
             System.out.println("Track: " + informacion.getTrack());
             System.out.println("Artist: " + informacion.getArtist());
             System.out.println("Title: " + informacion.getTitle());
@@ -28,52 +39,41 @@ public class MetaDatos1{
             System.out.println("Year: " + informacion.getYear());
             System.out.println("Genre: " + informacion.getGenre() + " (" + informacion.getGenreDescription() + ")");
             System.out.println("Comment: " + informacion.getComment());
-
-
+            
         }catch(Exception e){
             System.err.println("Exception caught: "+e);
         }
 
-		
-		
-	}
+        
+        
+    }
     public String gettitulo(){
         return informacion.getTitle();
     }
     public String getartista(){
         return informacion.getArtist();
-
-
-
     }
+
     public String getgenero(){
         return informacion.getTitle();
-
-
-
     }
+
     public String getalbum(){
         return informacion.getAlbum();
-
-
-
     }
+
     public String getyear(){
         return informacion.getYear();
-
-
-
     }
+
     public String gettrack(){
 
-        return informacion.getTitle();
-
-
-
+        return informacion.getTrack();
     }
+
+
     public static void main(String[] args) {
         MetaDatos1 prueba = new MetaDatos1();
-        System.out.println("Esta en una prueba de lo que imprime"+prueba.gettitulo());
         }
 
 }
