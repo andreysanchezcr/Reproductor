fb9c2c11be9e8ef7bc7039eaf8461f1e14b30f15
Andrey <andreysanchezcr@gmail.com>
2014-08-24 23:05:25 -0600

    Version 1 de interfaz sujeta a errores

---
+++ Interfaz.java
@@ -0,0 +1,119 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+
+
+public class Interfaz extends JFrame// implements ActionListener// implements java.awt.ActionListener
+{
+
+	String estadoM;
+	JButton BtnPlay;//Boton para reproducir musica
+	JButton BtnStop;
+	
+	JButton BtnAbrir;
+	//JTextField text;
+	JLabel l1;
+	JSlider volumen;
+	public Interfaz(){
+		super("Reproductor");
+		this.setBounds(25,56,200,300);//Dimensiones
+		this.setVisible(true);//Hace visible la Interfaz
+		this.setLayout(null);//Quita el Layout
+		this.setSize(900,900);// Dimensiones de la Interfaz
+		this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		cargar();
+
+
+
+	}
+
+	public void cargar(){
+		//eventoPlay evento = new eventoPlay();
+		BtnStop=new JButton("Pausar");
+		BtnAbrir=new JButton("Abrir");
+		BtnPlay = new JButton("Reproducir");
+		volumen = new javax.swing.JSlider();
+		BtnPlay.setBounds(400,50,150,50);//setBounds(x,y,ancho,largo)**Sirve para dar Atributos al boton
+		BtnStop.setBounds(200,50,150,50);//setBounds(x,y,ancho,largo)**Sirve para dar Atributos al boton
+		BtnAbrir.setBounds(400,100,150,50);//setBounds(x,y,ancho,largo)**Sirve para dar Atributos al boton
+		volumen.setBounds(200,100,170,100);
+		/*BtnPlay.addActionListener(new java.awt.event.ActionListener()){
+			@Override
+			public void actionPerformed(ActionEvent ae){
+				System.out.println("fsadfaf");
+			}
+		});*/
+		//BtnPlay.setActionCommand("BtnPlay");//Sirve para devolver un valor al listener
+		Evento evento= new Evento();
+		BtnPlay.addActionListener(evento);
+		BtnStop.addActionListener(evento);
+		BtnAbrir.addActionListener(evento);
+
+
+		this.add(BtnPlay);
+		this.add(BtnStop);
+		this.add(BtnAbrir);
+		this.add(volumen);
+
+		//text=new JTextField();
+		//text.setBounds(100,100,100,100);
+		//this.add(text);
+		l1=new JLabel("Volumen");
+		l1.setBounds(400,0,200,25);
+		this.add(l1);
+
+	}
+
+		
+			
+
+
+
+       //Main de prueba
+
+     /*
+	public static void main(String[] args) {
+		//System.out.println("fsaffa");
+		Interfaz Interfaz1 = new Interfaz();
+		//Interfaz1.setSize(200,200);
+	}
+
+    */
+
+			
+
+
+		
+	public class Evento implements ActionListener{
+
+
+
+		public void  actionPerformed(ActionEvent event){
+			String string ="";
+
+			if(event.getSource()==BtnPlay){
+
+				System.out.println("Estas dentro del boton 1");
+			}
+			if(event.getSource()==BtnStop){
+
+				System.out.println("Boton pausa");// Sustituir este campo con las funciones correspondientes
+			}
+			if(event.getSource()==BtnAbrir){
+
+				System.out.println("Boton abrir");
+				}	
+			//else if(even.getSource()==BtnPlay)
+			
+
+
+		}
+
+
+
+		
+
+
+	}
+}
+
