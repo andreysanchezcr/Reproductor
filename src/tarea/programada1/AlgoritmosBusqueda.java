6fb88939ba9d08e909916abff7558c40fedf0250
ricardo <ricardo@192.168.0.8>
2014-09-04 00:59:19 -0600

    Version 1.0 Reproductor

---
+++ src/tarea/programada1/AlgoritmosBusqueda.java
@@ -0,0 +1,48 @@
+package tarea.programada1;
+
+/**
+* Esta clase permite la busqueda de datos utilizando la clase Datos
+*/
+
+public class AlgoritmosBusqueda{
+
+	Datos cajaDatos;
+
+	public AlgoritmosBusqueda(Datos cajaDatos){
+
+		this.cajaDatos = cajaDatos;
+
+	}
+
+	public String buscarCancion(String nombre, int tipo){
+
+		for(int contador = 0;contador < this.cajaDatos.getTamaño();contador++){
+			String variable = this.cajaDatos.obtenerDatos(contador)[tipo];
+
+			if(variable == nombre){
+				return this.cajaDatos.obtenerDatos(contador)[tipo];
+			}
+		}
+		return null;
+	}
+        public static void main(String[] args){
+            String[] array = new String [3];//{"R","N","A"};
+            String[] array1 = {"","",""};
+            array[0]="R";
+            array[1]="Naaaa";
+            array[2]="A";
+                    
+            Datos a = new Datos(array1);
+            a.añadirDatos(array);
+            //a.añadirDatos(array);
+            AlgoritmosBusqueda b = new AlgoritmosBusqueda(a);
+            if(b.buscarCancion("Naaaa", 1)!=null)
+                            System.out.println("Entro");
+
+            
+            
+            
+            
+        }
+
+}
